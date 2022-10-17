d09f4498c5c0116d43ee4483c64f5085bd4207c6
rtx <rtx>
2022-10-17 17:32:38 +0600

    Update 2022-10-17T11:32:29.201Z

--- src/js/import/modules.js
+++
@@ -1,3 +0,0 @@
-import "../../../node_modules/jquery/dist/jquery.min.js"
-import "../../../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"
-import "../../../node_modules/swiper/swiper-bundle.js"
