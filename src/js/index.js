0b2144965a8f3cb98db52053416bcf36b8d81df0
Rtxrulez <rtx@rtx.ru>
2022-10-24 19:52:09 +0600

    autoplay

--- src/js/index.js
+++ src/js/index.js
@@ -1,46 +1,49 @@
 import './import/modules';
-import Swiper from 'swiper';
+import Swiper, {Autoplay} from 'swiper';
 
-const buttons = document.querySelectorAll('.card-list__item');
-const sections = document.querySelectorAll('.card-info');
+document.addEventListener('DOMContentLoaded', () => {
+  const buttons = document.querySelectorAll('.card-list__item');
+  const sections = document.querySelectorAll('.card-info');
 
-buttons.forEach((btn) => {
-  btn.addEventListener('click', () => {
-    buttons.forEach((btn) => {
-      btn.classList.remove('active');
+  buttons.forEach((btn) => {
+    btn.addEventListener('click', () => {
+      buttons.forEach((btn) => {
+        btn.classList.remove('active');
+      });
+      btn.classList.add('active');
+      const id = btn.id;
+      sections.forEach((section) => {
+        section.classList.remove('active');
+      });
+      const req = document.getElementsByClassName(`card-info${id}`);
+      req[0].classList.add('active');
     });
-    btn.classList.add('active');
-    const id = btn.id;
-    sections.forEach((section) => {
-      section.classList.remove('active');
-    });
-    const req = document.getElementsByClassName(`card-info${id}`);
-    req[0].classList.add('active');
+  });
+
+  const swiper = new Swiper('.swiper', {
+    modules: [Autoplay],
+    autoplay: {
+      delay: 3000,
+    },
+    loop: true,
+    slidesPerView: 1,
+    speed: 1000,
+    spaceBetween: 10,
+
+    breakpoints: {
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
+
+      1200: {
+        slidesPerView: 3,
+        spaceBetween: 20,
+      },
+    },
+  });
+
+  $('.burger, .close').on('click', () => {
+    $('.header').toggleClass('open');
   });
 });
-
-const swiper = new Swiper('.swiper', {
-  loop: true,
-  slidesPerView: 1,
-  autoplay: {
-    delay: 5000,
-  },
-  spaceBetween: 10,
-
-  breakpoints: {
-    768: {
-      slidesPerView: 2,
-      spaceBetween: 20,
-    },
-
-    1200: {
-      slidesPerView: 3,
-      spaceBetween: 20,
-    },
-  },
-});
-
-$('.burger, .close').on('click', () => {
-  $('.header').toggleClass('open');
-});
-
